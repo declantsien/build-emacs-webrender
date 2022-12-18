d8094d3a270c70654f0ab0692afb3a5666e33ac5
Declan Tsien <declantsien@riseup.net>
2022-12-18 15:07:16 +0800

    update gh repo

--- pkg/cli/plan.go
+++ pkg/cli/plan.go
@@ -32,7 +32,7 @@ func planCmd() *cli2.Command {
 					"tarball from",
 				Aliases: []string{"e"},
 				EnvVars: []string{"EMACS_REPO"},
-				Value:   "emacs-mirror/emacs",
+				Value:   "declantsien/emacs-wr",
 			},
 			&cli2.StringFlag{
 				Name:  "sha",
